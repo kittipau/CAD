38d0198f34a412eed1810d04d698b29a0e35f895
pulpracticas.VSTI <pulpracticas.VSTI@VSTIPTPB0122.grupoversia.local>
2022-05-25 14:27:04 +0200

    pojos

--- src/pojos/Participante.java
+++ src/pojos/Participante.java
@@ -10,7 +10,7 @@ import java.awt.Image;
  *
  * @author pulpracticas.VSTI
  */
-class Participante {
+public class Participante {
 
     private Integer participanteID;
     private String nombreDiseno;
